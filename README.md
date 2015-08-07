8515a6ad918756dd4857ab95271792f7d13ebe15
Don <don.j.olmstead@gmail.com>
2015-08-06 23:58:44 -0700

    Initial commit of library

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# dogma-drone-api
-API client for the Drone Continuous Integration Service using Dart
+# Dogma Drone API
+> API client for the Drone Continuous Integration Service using Dart
---
+++ build.dart
@@ -0,0 +1,28 @@
+// Copyright (c) 2015, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+library dogma_drone_api.build_system;
+
+//---------------------------------------------------------------------
+// Imports
+//---------------------------------------------------------------------
+
+import 'package:dogma_json_schema/build.dart';
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+void main(List<String> args) {
+  build(args, 'dogma_drone_api', 'lib/models.json', header: _header);
+}
+
+/// Header for the generated source files.
+const String _header =
+'''//
+// Copyright (c) 2015, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.''';
