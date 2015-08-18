a85ebcf2b7600c4fadbc5584b3a2d1ef168976f2
Don <don.j.olmstead@gmail.com>
2015-08-17 23:08:19 -0700

    Adding back in build.sh

---
+++ build.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/dogma-dart/dogma-codegen.git ../dogma-codegen
+git clone https://github.com/dogma-dart/dogma-data.git ../dogma-data
+git clone https://github.com/dogma-dart/dogma-json-schema.git ../dogma-json-schema
+
+pub install
+dart build.dart
+
+pub global activate linter
+pub global run linter .
