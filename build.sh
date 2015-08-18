ae6e1aa6de2c4782dec5c4b086b39ffbc14ba0da
Don <don.j.olmstead@gmail.com>
2015-08-17 23:00:27 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+# \TODO Remove git dependencies
 git clone https://github.com/dogma-dart/dogma-data.git ../dogma-data
 git clone https://github.com/dogma-dart/dogma-codegen.git ../dogma-codegen
 git clone https://github.com/dogma-dart/dogma-json-schema.git ../dogma-json-schema
