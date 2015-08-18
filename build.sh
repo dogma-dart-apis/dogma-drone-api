4c8b3e9c503e3bbffb040fb15e52c98b7a77c19c
Don <don.j.olmstead@gmail.com>
2015-08-17 22:37:47 -0700

    Fixing build

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
-echo "Clone dependencies"
 git clone https://github.com/dogma-dart/dogma-data.git ../dogma-data
 git clone https://github.com/dogma-dart/dogma-codegen.git ../dogma-codegen
 git clone https://github.com/dogma-dart/dogma-json-schema.git ../dogma-json-schema
 
-echo "Building library"
 pub install
 dart build.sh
