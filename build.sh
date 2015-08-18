71c9ab5b75955daebaf1acaa54c486cd8e887d5d
Don <don.j.olmstead@gmail.com>
2015-08-17 23:58:23 -0700

    Checking out specific branch

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/dogma-dart/dogma-codegen.git ../dogma-codegen
+git clone https://github.com/dogma-dart/dogma-codegen.git -b features/simplify-serialization ../dogma-codegen
 git clone https://github.com/dogma-dart/dogma-data.git ../dogma-data
 git clone https://github.com/dogma-dart/dogma-json-schema.git ../dogma-json-schema
 
