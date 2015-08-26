57965c554887939223d106c330092dbd0a1efdc5
Don <don.j.olmstead@gmail.com>
2015-08-25 22:51:44 -0700

    Adding dependency clones to build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@ git clone https://github.com/dogma-dart/dogma-codegen.git ../dogma-codegen
 git clone https://github.com/dogma-dart/dogma-data.git -b features/simplify-serialization ../dogma-data
 git clone https://github.com/dogma-dart/dogma-json-schema.git ../dogma-json-schema
 git clone https://github.com/dogma-dart/dogma-swagger.git ../dogma-swagger
+git clone https://github.com/dogma-dart/dogma-mapper.git ../dogma-mapper
+git clone https://github.com/dogma-dart/dogma-connection.git ../dogma-connection
+git clone https://github.com/dogma-dart/dogma-rest-client.git ../dogma-rest-client
+git clone https://github.com/dogma-dart/dogma-rest-connection.git ../dogma-rest-connection
 
 pub install
 dart build.dart
