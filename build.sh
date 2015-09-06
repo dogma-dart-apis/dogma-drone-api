e63fb6ce902a8a3b48dff1e4303c73ffe035dfd2
Don <don.j.olmstead@gmail.com>
2015-09-06 10:29:38 -0700

    Fixing build

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 git clone https://github.com/dogma-dart/dogma-codegen.git ../dogma-codegen
 git clone https://github.com/dogma-dart/dogma-data.git ../dogma-data
 git clone https://github.com/dogma-dart/dogma-json-schema.git ../dogma-json-schema
