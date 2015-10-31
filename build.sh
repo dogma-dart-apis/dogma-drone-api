15f8f3377d52de842b5098d77ed388f6cac797d6
Don <don.j.olmstead@gmail.com>
2015-10-31 09:02:21 -0700

    Update build script

--- build.sh
+++ build.sh
@@ -3,13 +3,8 @@ set -ex
 
 # Clone dependencies
 git clone https://github.com/dogma-dart/dogma-codegen.git ../dogma-codegen
-git clone https://github.com/dogma-dart/dogma-data.git ../dogma-data
 git clone https://github.com/dogma-dart/dogma-json-schema.git ../dogma-json-schema
 git clone https://github.com/dogma-dart/dogma-swagger.git ../dogma-swagger
-git clone https://github.com/dogma-dart/dogma-mapper.git ../dogma-mapper
-git clone https://github.com/dogma-dart/dogma-connection.git ../dogma-connection
-git clone https://github.com/dogma-dart/dogma-rest-client.git ../dogma-rest-client
-git clone https://github.com/dogma-dart/dogma-rest-connection.git ../dogma-rest-connection
 
 # Get version
 dart --version
